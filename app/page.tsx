c7af2cce1dc6f6bfeed258ff82978666039d7cd2
Darshan <darshanarkhade@gmail.com>
2023-06-09 19:00:34 +0530

    landing page started

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,78 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      
+      <nav className='flex flex-row justify-between bg-black px-4 w-full h-20 items-center fixed'>
+        <div className='px-5 flex flex-row justify-start items-center'>
+          <Image src="/images/logo.png" alt="logo" width={70} height={70} />
+          <h1 className='text-white text-3xl font-semibold px-3'>DegChain</h1>
+        </div>
+        <div className='flex flex-row justify-evenly basis-1/4' >
+          <button className='bg-white h-8 px-3 rounded-full m-1'>Register</button>
+          <button className='bg-white h-8 px-3 rounded-full m-1'>Login</button>
+        </div>
+      </nav>
+
+      <div className="flex items-center  bg-cover  w-full h-screen bg-center bg-no-repeat bg-local 
+      text-white  text-4xl pl-4 capitalize font-bold " 
+      style={{ backgroundImage: 'url("/images/background.png")' }}>
+        Intuitive Blockchain 
+
+        powered Document 
+
+        management system
+      </div>
+
+      <div className='h-98 bg-black rounded-t-3xl mt-2'>
+        <h2 className='text-white  text-center pt-10 text-2xl font-medium'>
+         The Document Management Systems Infrastructure:
+        </h2>
+        <div className='flex flex-row flex-wrap justify-around gap-4 items-start p-10 '>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center gap-2  '>
+              <Image src="/images/image1.png" alt="" width={200} height={200}/>
+              <p className='text-white font-medium'>Web UI for administrators</p>
+          </div>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center gap-2 '>
+              <Image src="/images/image2.png" alt="" width={150} height={200} />
+              <p className='text-white font-medium'>Database</p>
+          </div>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center gap-2 '>
+              <Image src="/images/image3.png" alt="" width={200} height={200}/>
+              <p className='text-white font-medium'>Secure API gateway for data access</p>
+          </div>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center gap-2 '>
+              <Image src="/images/image4.png" alt="" width={200} height={200}/>
+              <p className='text-white font-medium '>User interface for document storage and access</p>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='h-98 bg-white '>
+        <h2 className='text-black text-center pt-10 text-2xl font-medium'>
+         DegChains Features:
+        </h2>
+        <div className='flex flex-row flex-wrap justify-around gap-4 items-start p-10 '>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center text-center gap-2'>
+              <Image src="/images/feature1.png" alt="" width={60} height={60} className='justify-start'/>
+              <p className='text-black'>System instance setup</p>
+          </div>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center text-center gap-2'>
+              <Image src="/images/feature2.png" alt="" width={60} height={60} className='justify-start'/>
+              <p className='text-black'>Dedicated accounts and login for admins and users</p>
+          </div>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center text-center gap-2'>
+              <Image src="/images/feature3.png" alt="" width={60} height={60}/>
+              <p className='text-black'>Invite, manage, and remove users</p>
+          </div>
+          <div className='flex flex-col basis-1/5 min-w-48 items-center text-center gap-2'>
+              <Image src="/images/feature4.png" alt="" width={60} height={60}/>
+              <p className='text-black '>Gateways with traffic certification support and blockchain
+data confirmation</p>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        '128': '32rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
