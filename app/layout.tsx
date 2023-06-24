9f289c6ee60fbf79f0f609f683aadbf1fc302f70
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-06-24 18:34:54 +0530

    set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
 export default function RootLayout({
     children,
@@ -9,8 +11,12 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <html lang="en">
+                    <body className={inter.className}>{children}</body>
+                </html>
+            </NotificationProvider>
+        </MoralisProvider>
     );
 }
